d07e40bec011403501217c6e3c684bc507deacbc
Tim <tim.schumacher@nokia.com>
2017-12-11 13:34:10 -0500

    Trying to use a webform

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 import menu_gen
 import recipe as drink_recipe
+
+# app config
+app = Flask(__name__)
+app.config.from_object(__name__)
+with open('local_secret') as fp:
+    app.config['SECRET_KEY'] = fp.read().strip()
+
 
 class MixMindServer():
     def __init__(self):
@@ -13,19 +20,39 @@ class MixMindServer():
 
 mms = MixMindServer()
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
 
-@app.route('/Martini')
-def martini():
-    return str(mms.recipes['Martini'])
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+
+    def reset(self):
+        blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
+        self.process(blankData)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+
+    print form.errors
+    if request.method == 'POST':
+        name = request.form['name']
+        print name
+
+        if form.validate():
+            # Save the comment here.
+            flash('Hello ' + name)
+        else:
+            flash('All the form fields are required. ')
+
+    return render_template('hello.html', form=form)
+
 
 @app.route('/json/<recipe_name>')
-def recipe_json():
-    print recipe_name
+def recipe_json(recipe_name):
     try:
         return str(mms.recipes[recipe_name])
     except KeyError:
         return "{} not found".format(recipe_name)
+
+
 
