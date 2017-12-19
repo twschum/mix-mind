d74f67c671a91eb12c295daf82808438e10ac0be
Tim <tim.schumacher@nokia.com>
2017-12-19 11:59:33 -0500

    Adds the filtering form fields

--- hello.py
+++ hello.py
@@ -26,9 +26,6 @@ class ReusableForm(Form):
     email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
     password = TextField('Passwords:', validators=[validators.required(), validators.Length(min=3, max=35)])
 
-    def reset(self):
-        blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
-        self.process(blankData)
 
 class ToggleField(BooleanField):
     def __call__(self, **kwargs):
@@ -39,6 +36,21 @@ class ToggleField(BooleanField):
                 data_width="300",
                 **kwargs)
 
+class CSVField(Field):
+    widget = TextInput()
+
+    def _value(self):
+        if self.data:
+            return u', '.join(self.data)
+        else:
+            return u''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = [x.strip() for x in valuelist[0].split(',')]
+        else:
+            self.data = []
+
 class ToggleButtonWidget(widgets.Input):
     pass
 
@@ -48,35 +60,40 @@ class DrinksForm(Form):
     prep_line = BooleanField("Preparation", description="Display a line showing glass, ice, and prep")
     stats = BooleanField("Stats", description="Print out a detailed statistics block for the selected recipes")
     examples = BooleanField("Examples", description="Show specific examples of a recipe based on the ingredient stock")
+    all_ingredients = BooleanField("All Ingredients", description="Show every ingredient instead of just the main liquors with each example")
     convert = TextField("Convert", description="Convert recipes to a different primary unit", default=None, validators=[validators.AnyOf(['oz','mL','cL']), validators.Optional()])
-    all_ingredients = BooleanField("All_ingredients", description="Show every ingredient instead of just the main liquors with each example")
     markup = DecimalField("Markup", description="Drink markup: price = ceil((base_cost+1)*markup)", default=1.2)
-    ignore_info = BooleanField("Ignore_info", description="Don't show the info line for recipes", default="false")
-    ignore_origin = BooleanField("Ignore_origin", description="Don't check origin and mark drinks as Schubar originals")
-    ignore_variants = BooleanField("Ignore_variants", description="Don't show variants for drinks")
+    ignore_info = BooleanField("Info", description="Show the info line for recipes")
+    ignore_origin = BooleanField("Origin", description="Check origin and mark drinks as Schubar originals", default=True)
+    ignore_variants = BooleanField("Variants", description="Show variants for drinks", default=True)
 
     # filtering options
-    #p.add_argument('-a', '--all', action='store_true', help="Include all ingredients from barstock whether or not that are marked in stock")
-    #p.add_argument('-i', '--include', nargs='+', help="Filter by ingredient(s) that must be contained in the recipe")
-    #p.add_argument('-x', '--exclude', nargs='+', help="Filter by ingredient(s) that must NOT be contained in the recipe")
-    #p.add_argument('--or', dest='use_or', action='store_true', help="use logical OR for included and excluded ingredient lists instead of default AND")
-    #p.add_argument('--style', help="Include drinks matching the style such as After Dinner or Longdrink")
-    #p.add_argument('--glass', help="Include drinks matching the style such as After Dinner or Longdrink")
-    #p.add_argument('--prep', help="Include drinks matching the style such as After Dinner or Longdrink")
-    #p.add_argument('--ice', help="Include drinks matching the style such as After Dinner or Longdrink")
+    all_ = BooleanField("Allow all ingredients", description="Include all ingredients from barstock whether or not that are marked in stock")
+    include = CSVField("Include", "Filter by ingredient(s) that must be contained in the recipe")
+    exclude = CSVField("Include", "Filter by ingredient(s) that must NOT be contained in the recipe")
+    use_or = BooleanField("Logical OR", "Use logical OR for included and excluded ingredient lists instead of default AND")
+    # TODO make these selection fields
+    style = TextField("Style", "Include drinks matching the style such as After Dinner or Longdrink")
+    glass = TextField("Glass", "Include drinks matching the style such as cocktail or rocks")
+    prep = TextField("Prep", "Include drinks matching the style such as shake or build")
+    ice = TextField("Ice", "Include drinks matching the ice such as crushed")
+
+    def reset(self):
+        blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
+        self.process(blankData)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     #form = ReusableForm(request.form)
     form = DrinksForm(request.form)
-    drink = None
+    recipes = []
 
 
     print form.errors
     if request.method == 'POST':
-        name=request.form['name']
-        password=request.form['password']
-        email=request.form['email']
+        name = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
         print name, " ", email, " ", password
 
         if form.validate():
@@ -84,9 +101,9 @@ def hello():
             flash('Thanks for registration ' + name)
             print request
         else:
-            flash('Error: All the form fields are required. ')
+            flash("Error: something")
 
-    return render_template('hello.html', form=form, drink=drink)
+    return render_template('hello.html', form=form, recipes=recipes)
 
 
 @app.route('/json/<recipe_name>')
