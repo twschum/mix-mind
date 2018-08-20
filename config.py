3ef61a24afd3e82f21a88e0f2826417f2050cc2d
Tim <tim.schumacher@nokia.com>
2018-08-20 12:44:31 -0400

    Use sqlalchemy engine URL gen function

--- config.py
+++ config.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+from sqlalchemy.engine.url import URL
+
 def get_config():
     if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
         return Config_GAE()
@@ -13,23 +15,25 @@ class Config(object):
     """ Configuration object
     """
     def __init__(self):
-        self.sqlalchemy_engine_cfg = {}
+        self.sqlalchemy_engine_params = {}
 
     # TODO cached property
     @property
     def sql_db_url(self):
         """ SQLAlchemy engine url
+        {dialect}+{driver}://{username}:{password}@{host}:{port}/{database}
         """
-        url_format = "{dialect}+{driver}://{username}:{password}@{host}:{port}/{database}"
-        return url_format.format(**self.sqlalchemy_engine_cfg)
+        if 'driver' and 'dialect' in self.sqlalchemy_engine_params:
+            kwargs['drivername'] = '{dialect}+{driver}'.format(**self.sqlalchemy_engine_params)
+            del kwargs['dialect']
+            del kwargs['driver']
+        return URL(**self.sqlalchemy_engine_params)
 
 class Config_dev(Config):
     def __init__(self):
         self.db_file = "db/auth.db"
+        self.sqlalchemy_engine_params['drivername'] = 'sqlite'
 
-    @property
-    def sql_db_url(self):
-        return "sqlite:///{}".format(self.db_file)
 
 class Config_GAE(Config):
     def __init__(self):
