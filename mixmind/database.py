258ff3b61a950019b1ee2220238c6ac2783abd86
Tim <tim.schumacher@nokia.com>
2018-08-25 13:11:05 -0400

    Add bartender user role

--- mixmind/database.py
+++ mixmind/database.py
@@ -11,4 +11,5 @@ def init_db():
     db.create_all()
     user_datastore.find_or_create_role(name='admin', description='An admin user may modify the parameters of the app backend')
     user_datastore.find_or_create_role(name='customer', description='Customer may register to make it easier to order drinks')
+    user_datastore.find_or_create_role(name='bartender', description='This user is a bartender at at least one bar')
     db.session.commit()
