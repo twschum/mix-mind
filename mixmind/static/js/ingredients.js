d47df11bee3c8686c866146b4bb87cef8e73f122
Tim <tim.schumacher@nokia.com>
2018-09-13 10:36:52 -0400

    Pull out the column settings so they can be used for the callback

--- mixmind/static/js/ingredients.js
+++ mixmind/static/js/ingredients.js
@@ -7,32 +7,34 @@ $.fn.dataTable.render.money_fmt = function(data, type, row, meta) {
     return data;
 }
 
-function editCell (updatedCell, updatedRow, oldValue) {
-    console.log("The new value for the cell is: " + updatedCell.data());
-}
+var column_settings = [
+    {data: "Category", name: "Category"}, // TODO use an enum def to sort by caregory
+    {data: "Type", name: "Type"},
+    {data: "Bottle", name: "Bottle"},
+    {data: "In_Stock", name: "In_Stock"},
+    {data: "Proof", name: "Proof", className: "text-right monospace", render: function(data, type, row, meta){
+        if (type === "display"){
+            if (data == 0) {
+                return "&mdash;"
+            }
+            return ((data / 2.0)+0.01).toFixed(1) + " %";
+        }
+        return data;
+    }},
+    {data: "Size_oz", name: "Size_oz",
+        className: "text-right monospace", render: $.fn.dataTable.render.number('','.',1,''," oz")},
+    {data: "Price_Paid", name: "Price_Paid",
+        className: "text-right monospace", render: $.fn.dataTable.render.money_fmt},
+    {data: "Cost_per_oz", name: "Cost_per_oz",
+        className: "text-right monospace", render: $.fn.dataTable.render.money_fmt}
+]
 
+var barstock_table;
 $(document).ready( function () {
-    var barstock_table = $("#barstock-table").DataTable( {
+    barstock_table = $("#barstock-table").DataTable( {
         "paging": false,
         "ajax": "/api/load_ingredients",
-        "columns": [
-            {data: "Category"}, // TODO use an enum def to sort by caregory
-            {data: "Type"},
-            {data: "Bottle"},
-            {data: "In_Stock"},
-            {data: "Proof", className: "text-right monospace", render: function(data, type, row, meta){
-                if (type === "display"){
-                    if (data == 0) {
-                        return "&mdash;"
-                    }
-                    return ((data / 2.0)+0.01).toFixed(1) + " %";
-                }
-                return data;
-            }},
-            {data: "Size_oz", className: "text-right monospace", render: $.fn.dataTable.render.number('','.',1,''," oz")},
-            {data: "Price_Paid", className: "text-right monospace", render: $.fn.dataTable.render.money_fmt},
-            {data: "Cost_per_oz", className: "text-right monospace", render: $.fn.dataTable.render.money_fmt}
-        ]
+        "columns": column_settings
     });
     // editCell integration
     barstock_table.MakeCellsEditable({
@@ -47,4 +49,9 @@ $(document).ready( function () {
     })
 } );
 
-
+function editCell (cell, row, oldValue) {
+    console.log("The new value for the cell is: " + cell.data());
+    if (cell.data() == oldValue) {
+        return null;
+    }
+}
