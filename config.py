0de20ce97e1146320c1a05ccf822d34500135e84
Tim <tim.schumacher@nokia.com>
2018-10-19 10:03:35 -0400

    Move reference barstock file

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ SECURITY_EMAIL_SUBJECT_CONFIRM = "[Mix-Mind] Please confirm your email"
 # default recipes and ingredients to load
 MIXMIND_DIR = os.path.abspath(os.path.curdir)
 MIXMIND_RECIPES_DIR = "recipes/"
-MIXMIND_INGREDIENTS_DIR = "ingredients/"
+MIXMIND_INGREDIENTS_DIR = "mixmind/static/ingredients/"
 
 MIXMIND_DEFAULT_RECIPES = ["recipes_schubar.json", "IBA_all.json"]
 MIXMIND_DEFAULT_INGREDIENTS = ["12BBplus.csv"]
